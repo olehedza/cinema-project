c982d257fe7c9dd11d2abdf17811e631eb7da28e
Oleksii <olehedza@gmail.com>
2020-05-21 13:35:56 +0300

    add Movie model, dao & service layers (#1)
    * add Movie model, dao & service layers
    
    * rename itemId to movieId
    
    * add empty line to the end of file
    
    * add access modifier

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,7 +1,22 @@
 package com.dev.cinema;
 
-public class Main {
-    public static void main(String[] args) {
+import com.dev.cinema.lib.Injector;
+import com.dev.cinema.model.Movie;
+import com.dev.cinema.service.MovieService;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("com.dev.cinema");
+
+    public static void main(String[] args) {
+        Movie movie1 = new Movie();
+        Movie movie2 = new Movie();
+        movie1.setTitle("Die hard");
+        movie2.setTitle("Once upon a time in Hollywood");
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
+        movieService.add(movie1);
+        movieService.add(movie2);
+
+        movieService.getAll().forEach(System.out::println);
     }
 }
