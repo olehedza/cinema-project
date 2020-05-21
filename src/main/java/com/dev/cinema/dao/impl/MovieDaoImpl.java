f8dfc41db834d33d40bce239e811aada00d73bb7
olehedza <olehedza@gmail.com>
2020-05-21 20:12:35 +0300

    replace try with resources by try-catch-finally

--- src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/MovieDaoImpl.java
@@ -21,7 +21,9 @@ public class MovieDaoImpl implements MovieDao {
     @Override
     public Movie add(Movie movie) {
         Transaction transaction = null;
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
             Long movieId = (Long) session.save(movie);
             transaction.commit();
@@ -32,6 +34,10 @@ public class MovieDaoImpl implements MovieDao {
                 transaction.rollback();
             }
             throw new RuntimeException("Can't insert Movie entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
