fc5ee159cff24e77ccba7ce7c772d920ab191a3d
olehedza <olehedza@gmail.com>
2020-05-26 00:53:12 +0300

    add dao layer for user model

---
+++ src/main/java/com/dev/cinema/dao/impl/UserDaoImpl.java
@@ -0,0 +1,51 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.UserDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.model.User;
+import com.dev.cinema.util.HibernateUtil;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
+
+public class UserDaoImpl implements UserDao {
+    private static final Logger LOGGER = Logger.getLogger(MovieDaoImpl.class);
+
+    @Override
+    public User add(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+            LOGGER.info(String.format("insert User entity #%d to DB", user.getId()));
+            return user;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert User entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<User> userQuery = session.createQuery("from User where email = :email", User.class);
+            userQuery.setParameter("email", email);
+            return userQuery.uniqueResultOptional();
+        } catch (Exception e) {
+            throw new DataProcessingException(String
+                    .format("Can't get User by email: %s", email), e);
+        }
+    }
+}
