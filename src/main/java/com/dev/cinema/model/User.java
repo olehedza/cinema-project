dbeabd7922df0e40ea1d4c41b31316907f7f7f67
olehedza <olehedza@gmail.com>
2020-05-26 20:04:14 +0300

    add unique constraint for email column

--- src/main/java/com/dev/cinema/model/User.java
+++ src/main/java/com/dev/cinema/model/User.java
@@ -1,5 +1,6 @@
 package com.dev.cinema.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String email;
     private String password;
     private byte[] salt;
