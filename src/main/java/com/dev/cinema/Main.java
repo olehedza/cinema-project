cfee83593ebaabb98754d1de4e34eae0fbc2dc5a
olehedza <olehedza@gmail.com>
2020-05-26 21:21:45 +0300

    add testing code

--- src/main/java/com/dev/cinema/Main.java
+++ src/main/java/com/dev/cinema/Main.java
@@ -1,20 +1,27 @@
 package com.dev.cinema;
 
+import com.dev.cinema.exceptions.AuthenticationException;
 import com.dev.cinema.lib.Injector;
 import com.dev.cinema.model.CinemaHall;
 import com.dev.cinema.model.Movie;
 import com.dev.cinema.model.MovieSession;
+import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.User;
+import com.dev.cinema.security.AuthenticationService;
 import com.dev.cinema.service.CinemaHallService;
 import com.dev.cinema.service.MovieService;
 import com.dev.cinema.service.MovieSessionService;
+import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import com.dev.cinema.util.HashUtil;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.List;
+import org.apache.log4j.Logger;
 
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
     private static final Injector injector = Injector.getInstance("com.dev.cinema");
     private static final MovieService movieService =
             (MovieService) injector.getInstance(MovieService.class);
@@ -24,6 +31,10 @@ public class Main {
             (MovieSessionService) injector.getInstance(MovieSessionService.class);
     private static final UserService userService =
             (UserService) injector.getInstance(UserService.class);
+    private static final AuthenticationService authService =
+            (AuthenticationService) injector.getInstance(AuthenticationService.class);
+    private static final ShoppingCartService cartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
 
     public static void main(String[] args) {
         Movie movie1 = new Movie();
@@ -85,5 +96,36 @@ public class Main {
         user.setPassword(HashUtil.getPasswordDigest("1234", salt));
         userService.add(user);
         System.out.println(userService.findByEmail("user@google.com"));
+
+        try {
+            authService.register("nin@gmail.com", "industrial");
+            authService.register("manson@gmail.com", "shock");
+            authService.register("rammstein@yahoo.com", "neuedeutscheharte");
+        } catch (AuthenticationException e) {
+            LOGGER.error("register error", e);
+        }
+
+        try {
+            User trentReznor = authService.login("nin@gmail.com", "industrial");
+            User marilynManson = authService.login("manson@gmail.com", "shock");
+            User tillLindemann = authService
+                    .login("rammstein@yahoo.com", "neuedeutscheharte");
+            ShoppingCart cart1 = cartService.getByUser(trentReznor);
+            ShoppingCart cart2 = cartService.getByUser(marilynManson);
+            ShoppingCart cart3 = cartService.getByUser(tillLindemann);
+            List<ShoppingCart> cartList = List.of(cart1, cart2, cart3);
+            cartList.forEach(System.out::println);
+
+            cartService.addSession(movieSessionOne, trentReznor);
+            cartService.addSession(movieSessionTwo, marilynManson);
+            cartService.addSession(movieSessionThree, tillLindemann);
+            cart1 = cartService.getByUser(trentReznor);
+            cart2 = cartService.getByUser(marilynManson);
+            cart3 = cartService.getByUser(tillLindemann);
+            List<ShoppingCart> carts = List.of(cart1, cart2, cart3);
+            carts.forEach(System.out::println);
+        } catch (AuthenticationException e) {
+            LOGGER.error("login error", e);
+        }
     }
 }
