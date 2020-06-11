101f5e516f37aeafa96adfbf7b3eaa22bf8ff97d
olehedza <olehedza@gmail.com>
2020-06-11 19:56:18 +0300

    add annotation and DI in AuthenticationServiceImpl

--- src/main/java/com/dev/cinema/security/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/dev/cinema/security/impl/AuthenticationServiceImpl.java
@@ -6,26 +6,22 @@ import com.dev.cinema.security.AuthenticationService;
 import com.dev.cinema.service.ShoppingCartService;
 import com.dev.cinema.service.UserService;
 import com.dev.cinema.util.HashUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
+    private final HashUtil hashUtil;
     private final UserService userService;
     private final ShoppingCartService cartService;
-
-    @Autowired
-    public AuthenticationServiceImpl(UserService userService, ShoppingCartService cartService) {
-        this.userService = userService;
-        this.cartService = cartService;
-    }
 
     @Override
     public User login(String email, String password) throws AuthenticationException {
         User userFromDB = userService.findByEmail(email);
 
         if (userFromDB != null
-                && HashUtil.getPasswordDigest(password, userFromDB.getSalt())
+                && hashUtil.getPasswordDigest(password, userFromDB.getSalt())
                 .equals(userFromDB.getPassword())) {
             return userFromDB;
         }
@@ -41,8 +37,8 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
         User user = new User();
         user.setEmail(email);
-        user.setSalt(HashUtil.getSalt());
-        user.setPassword(HashUtil.getPasswordDigest(password, user.getSalt()));
+        user.setSalt(hashUtil.getSalt());
+        user.setPassword(hashUtil.getPasswordDigest(password, user.getSalt()));
         User userFromDB = userService.add(user);
         cartService.registerNewShoppingCart(userFromDB);
         return userFromDB;
