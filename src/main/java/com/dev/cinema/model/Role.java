cb1b766a17333342a8cc85435c03abca16d2bc5f
olehedza <olehedza@gmail.com>
2020-06-17 15:55:40 +0300

    add Role class

---
+++ src/main/java/com/dev/cinema/model/Role.java
@@ -0,0 +1,30 @@
+package com.dev.cinema.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "roles")
+@Data
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    @Column(unique = true)
+    private RoleName name;
+
+    public enum RoleName {
+        USER,
+        ADMIN
+    }
+}
